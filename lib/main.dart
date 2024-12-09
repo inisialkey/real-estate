7a3a6e840e979f3f4b6091391481dde67b077ea6
inisialkey <okidorvel404@gmail.com>
2024-12-09 10:23:50 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:real_estate/common/app_color.dart';
+import 'package:real_estate/pages/onboard_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xffF6F7F9),
+        primaryColor: AppColor.primary,
+        colorScheme: ColorScheme.light(primary: AppColor.primary),
+      ),
+      home: const OnBoardPage(),
+    );
+  }
+
+  buildNotFoundPage() {
+    return const Scaffold(
+      body: Center(
+        child: Text('Page Not Found'),
+      ),
+    );
+  }
+
+  buildInvalidArgumentPage() {
+    return const Scaffold(
+      body: Center(
+        child: Text('Invalid Argument'),
+      ),
+    );
+  }
+}
